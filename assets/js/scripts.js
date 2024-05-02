4f8d43881e76beb03760b362639798a7ea60b1fc
Ngozi Omenka <ngozi.madu@live.se>
2024-05-02 05:52:51 +0000

    Created question file and added all required elements and activated start game button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,223 +1,34 @@
-let questions = [
-    {
-        numb: 1,
-        question: "What country has the highest life expectancy?",
-        answer: "Hong Kong",
-        options: [
-            "United State",
-            "Italy",
-            "Malaysia",
-            "Hong Kong"
-        ]
-    },
-    {
-        numb: 2,
-        question: "Which langauge has the more native speakers?",
-        answer: "Spanish",
-        options: [
-            "English",
-            "Spanish",
-            "French",
-            "Chinese"
-        ]
-    },
-    {
-        numb: 3,
-        question: "What disease commonly spread on pirate ships?",
-        answer: "Scurvy",
-        options: [
-            "Maleria",
-            "Corona-virus",
-            "Scurvy",
-            "Flu"
-        ]
-    },
-    {
-        numb: 4,
-        question: "What year was the United Nations established?",
-        answer: "1945",
-        options: [
-            "1960",
-            "1975",
-            "1945",
-            "1990"
-        ]
-    },
-    {
-        numb: 5,
-        question: "How many minutes are in a full week?",
-        answer: "10,080",
-        options: [
-            "10,100",
-            "10,799",
-            "10,107",
-            "10080"
-        ]
-    },
-    {
-        numb: 6,
-        question: "Aureolin is a shade of what color?",
-        answer: "Yellow",
-        options: [
-            "Red",
-            "White",
-            "Yellow",
-            "Green"
-        ]
-    },
-    {
-        numb: 7,
-        question: "Which planet has the most moons?",
-        answer: "Saturn",
-        options: [
-            "Mercury",
-            "Saturn",
-            "Earth",
-            "Mars"
-        ]
-    },
-    {
-        numb: 8,
-        question: "What country has won the most World Cups?",
-        answer: "Saturn",
-        options: [
-            "Brazil",
-            "Germany",
-            "United States",
-            "Spain"
-        ]
-    },
-    {
-        numb: 9,
-        question: "In what country was Elon Musk born?",
-        answer: "South Africa",
-        options: [
-            "Austria",
-            "South Africa",
-            "United States",
-            "United Kingdom"
-        ]
-    },
-    {
-        numb: 10,
-        question: "Where is the strongest human muscle located?",
-        answer: "Jaw",
-        options: [
-            "Hand",
-            "Jaw",
-            "Leg",
-            "Tongue"
-        ]
-    },
-    {
-        numb: 11,
-        question: "How many hearts does an octopus have?",
-        answer: "Jaw",
-        options: [
-            "4",
-            "2",
-            "1",
-            "3"
-        ]
-    },
-    {
-        numb: 12,
-        question: "What planet is closest to the sun?",
-        answer: "Mercury",
-        options: [
-            "Earth",
-            "Mercury",
-            "Mars",
-            "Saturn"
-        ]
-    },
-    {
-        numb: 13,
-        question: "What phone company produced the 3310?",
-        answer: "Nokia",
-        options: [
-            "Samsong",
-            "Sony Ericsson",
-            "Nokia",
-            "Blackberry"
-        ]
-    },
-    {
-        numb: 14,
-        question: "What is the tallest type of tree?",
-        answer: "Redwoods",
-        options: [
-            "Banyan",
-            "Baobab",
-            "Birch",
-            "Redwoods"
-        ]
-    },
-    {
-        numb: 15,
-        question: "What colors is the flag of the United Nations?",
-        answer: "Blue and White",
-        options: [
-            "Blue and White",
-            "Blue and Red",
-            "White and Green",
-            "Blue and Yellow"
-        ]
-    },
-    {
-        numb: 16,
-        question: "What country drinks the most coffee?",
-        answer: "Finland",
-        options: [
-            "Sweden",
-            "Canada",
-            "Rusia",
-            "Finland"
-        ]
-    },
-    {
-        numb: 17,
-        question: "Where did sushi originate?",
-        answer: "China",
-        options: [
-            "China",
-            "India",
-            "Norway",
-            "Spain"
-        ]
-    },
-    {
-        numb: 18,
-        question: "How many dots appear on a pair of dice?",
-        answer: "42",
-        options: [
-            "64",
-            "34",
-            "43",
-            "42"
-        ]
-    },
-    {
-        numb: 19,
-        question: "How many bones do we have in an ear?",
-        answer: "3",
-        options: [
-            "3",
-            "2",
-            "4",
-            "1"
-        ]
-    },
-    {
-        numb: 20,
-        question: "What city is known as The Eternal City"?
-        answer: "Rome",
-        options: [
-            "New-York",
-            "England",
-            "Rome",
-            "Tarento"
-        ]
-    },
-]
+// Add all required elements
+let start_game = document.querySelector(".start_game button");
+let game_info_list = document.querySelector(".game_info_list");
+let exit_btn = game_info_list.querySelector(".buttons .exit");
+let continue_btn = game_info_list.querySelector(".buttons .continue");
+let quiz_box = document.querySelector(".quiz_box");
 
+// Activate start game button
+start_game.onclick = ()=>{
+    game_info_list.classList.add("activeInfo");
+};
+
+exit_btn.onclick = ()=>{
+    game_info_list.classList.remove("activeInfo");
+}
+
+continue_btn.onclick = ()=>{
+    game_info_list.classList.remove("activeInfo");
+    quiz_box.classList.add("activeQuiz");
+    showQuestions(0);
+}
+
+let que_count = 0;
+
+let next_btn = document.querySelector(".next_btn")
+
+next_btn.onclick = ()=>{
+    if (que_count < questions.length - 1){
+        que_count++;
+        showQuestions(que_count);
+    }else{
+        console.log("You have completed the questions")
+    }
+}
